94d10dcf3a2bd27e9073f57779a83c740e4c6a37
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-05-11 16:05:42 -0500

    [UP] Add gameboard

--- main.js
+++ main.js
@@ -1,4 +1,68 @@
-const boxes = document.querySelectorAll(".game-box");
+const gameBoard = [
+  { value: "" },
+  { value: "" },
+  { value: "" },
+  { value: "" },
+  { value: "" },
+  { value: "" },
+  { value: "" },
+  { value: "" },
+  { value: "" },
+];
+function checkWinCombinations() {
+  if (
+    gameBoard[0].value == gameBoard[1].value &&
+    gameBoard[0].value == gameBoard[2].value
+  ) {
+    console.log(`Winner player: ${gameBoard[0].value}`);
+  } else if (
+    gameBoard[3].value == gameBoard[4].value &&
+    gameBoard[3].value == gameBoard[5].value
+  ) {
+    console.log(`Winner player: ${gameBoard[3].value}`);
+  } else if (
+    gameBoard[6].value == gameBoard[7].value &&
+    gameBoard[6].value == gameBoard[8].value
+  ) {
+    console.log(`Winner player: ${gameBoard[6].value}`);
+  } else if (
+    gameBoard[0].value == gameBoard[3].value &&
+    gameBoard[0].value == gameBoard[6].value
+  ) {
+    console.log(`Winner player: ${gameBoard[0].value}`);
+  } else if (
+    gameBoard[1].value == gameBoard[4].value &&
+    gameBoard[1].value == gameBoard[7].value
+  ) {
+    console.log(`Winner player: ${gameBoard[1].value}`);
+  } else if (
+    gameBoard[2].value == gameBoard[5].value &&
+    gameBoard[2].value == gameBoard[8].value
+  ) {
+    console.log(`Winner player: ${gameBoard[2].value}`);
+  } else if (
+    gameBoard[0].value == gameBoard[4].value &&
+    gameBoard[0].value == gameBoard[8].value
+  ) {
+    console.log(`Winner player: ${gameBoard[0].value}`);
+  } else if (
+    gameBoard[2].value == gameBoard[4].value &&
+    gameBoard[2].value == gameBoard[6].value
+  ) {
+    console.log(`Winner player: ${gameBoard[2].value}`);
+  } else console.log("No one wins");
+}
+
+gameBoard[0].value = 0;
+gameBoard[1].value = 1;
+gameBoard[2].value = 0;
+gameBoard[3].value = 1;
+gameBoard[4].value = 0;
+gameBoard[5].value = 1;
+gameBoard[6].value = 0;
+gameBoard[7].value = 1;
+gameBoard[8].value = 1;
+checkWinCombinations();
 
 let turnOne = true;
 function markUP() {
@@ -16,20 +80,5 @@ function markUP() {
 function checkBoxes(box) {
   console.log(box.value);
 }
-
+const boxes = document.querySelectorAll(".game-box");
 boxes.forEach((box) => box.addEventListener("click", markUP));
-
-// function gameBoardConstructor(A1, A2, A3, B1, B2, B3, C1, C2, C3) {
-//   const [AOne, ATwo, AThree, BOne, BTwo, BThree, COne, CTwo, CThree] = [
-//     A1,
-//     A2,
-//     A3,
-//     B1,
-//     B2,
-//     B3,
-//     C1,
-//     C2,
-//     C3,
-//   ];
-//   console.log({ AOne, ATwo, AThree, BOne, BTwo, BThree, COne, CTwo, CThree });
-// }
