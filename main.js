71307e9b9a64f14554463b5a20713b5957dc9113
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-05-11 19:58:11 -0500

    [BUG] The game is over when a player wins

--- main.js
+++ main.js
@@ -15,59 +15,77 @@ function checkWinCombinations() {
     gameBoard[0].value === gameBoard[2].value
   ) {
     console.log(`Winner player: ${gameBoard[0].value}`);
+    gameStarted = false;
+    gameOver();
   } else if (
     gameBoard[3].value === gameBoard[4].value &&
     gameBoard[3].value === gameBoard[5].value
   ) {
     console.log(`Winner player: ${gameBoard[3].value}`);
+    gameStarted = false;
+    gameOver();
   } else if (
     gameBoard[6].value === gameBoard[7].value &&
     gameBoard[6].value === gameBoard[8].value
   ) {
     console.log(`Winner player: ${gameBoard[6].value}`);
+    gameStarted = false;
+    gameOver();
   } else if (
     gameBoard[0].value === gameBoard[3].value &&
     gameBoard[0].value === gameBoard[6].value
   ) {
     console.log(`Winner player: ${gameBoard[0].value}`);
+    gameStarted = false;
+    gameOver();
   } else if (
     gameBoard[1].value === gameBoard[4].value &&
     gameBoard[1].value === gameBoard[7].value
   ) {
     console.log(`Winner player: ${gameBoard[1].value}`);
+    gameStarted = false;
+    gameOver();
   } else if (
     gameBoard[2].value === gameBoard[5].value &&
     gameBoard[2].value === gameBoard[8].value
   ) {
     console.log(`Winner player: ${gameBoard[2].value}`);
+    gameStarted = false;
+    gameOver();
   } else if (
     gameBoard[0].value === gameBoard[4].value &&
     gameBoard[0].value === gameBoard[8].value
   ) {
     console.log(`Winner player: ${gameBoard[0].value}`);
+    gameStarted = false;
+    gameOver();
   } else if (
     gameBoard[2].value === gameBoard[4].value &&
     gameBoard[2].value === gameBoard[6].value
   ) {
     console.log(`Winner player: ${gameBoard[2].value}`);
-  } else console.log("No one wins");
+    gameStarted = false;
+    gameOver();
+  } else console.log("No one wins yet");
 }
 
 let turnOne = true;
 function markUP() {
-  turnOne ? (turnOne = !turnOne) : (turnOne = true);
-  if (turnOne) {
-    this.innerHTML = "x";
-    this.value = 1;
-    updateGameBoard(this.dataset.index, 1);
-  } else {
-    this.innerHTML = "0";
-    this.value = 0;
-    updateGameBoard(this.dataset.index, 0);
-  }
-  checkWinCombinations();
-  if (this.value == 1 || this.value == 0) {
-    this.removeEventListener("click", markUP);
+  if (gameStarted) {
+    turnOne ? (turnOne = !turnOne) : (turnOne = true);
+    if (turnOne) {
+      this.innerHTML = "x";
+      this.value = 1;
+      updateGameBoard(this.dataset.index, 1);
+    } else {
+      this.innerHTML = "0";
+      this.value = 0;
+      updateGameBoard(this.dataset.index, 0);
+    }
+    checkWinCombinations();
+    if (this.value == 1 || this.value == 0) {
+      this.removeEventListener("click", markUP);
+    }
   }
 }
 
@@ -76,11 +94,27 @@ function updateGameBoard(index, value) {
   // console.log(gameBoard);
 }
 
-let hasEvent = true;
-
 const boxes = document.querySelectorAll(".game-box");
-boxes.forEach((box) => {
-  if (box.value != 1 && box.value != 0) {
-    box.addEventListener("click", markUP);
+
+function activeBoxes() {
+  boxes.forEach((box) => {
+    if (box.value != 1 && box.value != 0) {
+      box.addEventListener("click", markUP);
+    }
+  });
+}
+
+function gameOver() {
+  console.log("GAME OVER");
+}
+
+let gameStarted = false;
+function startGame() {
+  !gameStarted ? (gameStarted = !gameStarted) : (gameStarted = false);
+  if (gameStarted) {
+    activeBoxes();
+  } else {
+    gameOver();
   }
-});
+}
+startGame();
