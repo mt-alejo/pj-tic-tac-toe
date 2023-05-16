5be132fa556097fdbee9a637ffb36eec480653ac
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-05-16 10:44:44 -0500

    [UP] Check for win manually

--- main.js
+++ main.js
@@ -46,10 +46,64 @@ const Game = (() => {
     }
     GameBoard.update(index, players[currentIndexPlayer].mark);
     currentIndexPlayer = currentIndexPlayer === 0 ? 1 : 0;
+    if (checkForWin(GameBoard.getGameboard())) {
+      gameOver = true;
+      alert(` wins!`);
+    }
   };
+
+  const restart = () => {};
 
   return { start, handleclick };
 })();
+
+const checkForWin = (gameboard) => {
+  const winCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // for (let i = 0; i < winCombination.length; i++) {
+  //   const [a, b, c] = winCombination[i];
+  //   if (
+  //     gameboard[a] &&
+  //     gameboard[a] === gameboard[b] &&
+  //     gameboard[a] === gameboard[c]
+  //   ) {
+  //     return true;
+  //   }
+  // }
+  // return false;
+  gameboard[0] && gameboard[0] === gameboard[1] && gameboard[0] === gameboard[2]
+    ? console.log("match")
+    : true;
+  gameboard[3] && gameboard[3] === gameboard[4] && gameboard[3] === gameboard[5]
+    ? console.log("match")
+    : true;
+  gameboard[6] && gameboard[6] === gameboard[7] && gameboard[6] === gameboard[8]
+    ? console.log("match")
+    : true;
+  gameboard[0] && gameboard[0] === gameboard[3] && gameboard[0] === gameboard[6]
+    ? console.log("match")
+    : true;
+  gameboard[1] && gameboard[1] === gameboard[4] && gameboard[1] === gameboard[7]
+    ? console.log("match")
+    : true;
+  gameboard[2] && gameboard[2] === gameboard[5] && gameboard[2] === gameboard[8]
+    ? console.log("match")
+    : true;
+  gameboard[0] && gameboard[0] === gameboard[4] && gameboard[0] === gameboard[8]
+    ? console.log("match")
+    : true;
+  gameboard[2] && gameboard[2] === gameboard[4] && gameboard[2] === gameboard[6]
+    ? console.log("match")
+    : true;
+};
 
 document.querySelector("#btn-start").addEventListener("click", () => {
   gameContainer.style.display = "grid";
